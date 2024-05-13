d8bcd95023f97f3693cfa367f74793948baeea2a
Wallison Batista <51002039+wallisonBatista@users.noreply.github.com>
2024-05-12 21:47:06 -0300

    add form control e corrigido erro de responsividade

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 export class FormsComponent {
 
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit():void {
+    console.log(this.formGroup.value)
+  }
+
+  onClean():void {
+    this.formGroup.reset();
+  }
 }
